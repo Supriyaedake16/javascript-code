2c4bc5d6d86e0bc777a3753f149367ae966e7b69
Supriyaedake16 <97042882+Supriyaedake16@users.noreply.github.com>
2024-09-25 16:42:05 +0000

    data type

---
+++ 01_basics/01_var.js
@@ -0,0 +1,12 @@
+const accountId = 13333
+let accountEmail="exa,ple@gmsil.com"
+var accountPass ="123"
+accountCity ="Solapur"
+let accountState; //use let if their is possiblity of change state
+/*
+prefer not use var because of issue in block scope and functional scope
+*/ 
+// accountId =2 //not allowed
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+//alert("hello") we are using nodejs, not browser 
+let name="supriya"
+let age=25
+let isLoggedIn =false 
+let state=null
+
+
+//number=>2 to power 53
+//bigint
+//string
+//boolen=>true false
+//null =>standalo value
+//undefined
+//symbol=>unique
+
+//object
+
+console.log(typeof "supriya");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
